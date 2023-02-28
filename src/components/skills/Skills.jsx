edfec674a5b6a47f7cfb962586d44dad76196b26
farrez30 <59773413+farrez30@users.noreply.github.com>
2023-02-28 21:56:17 +0700

    Menambahkan isi Portfolio, last update from Skills.jsx

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section class="skills section" id="skills">
+      <h2 className="section__title">About Me</h2>
+      <span className="section__subtitle">My introduction</span>
+    </section>
+  );
+};
+
+export default Skills;
