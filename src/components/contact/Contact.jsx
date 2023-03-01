d7c1190b8d93d178ba82e95a964dba803a1c5ecf
farrez30 <59773413+farrez30@users.noreply.github.com>
2023-03-01 22:11:34 +0700

    Adding Testimonials and Contact section

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section className="contact section" id="contact">
+      <h2 className="section__title">Get in touch</h2>
+      <span className="section__subtitle">Contact Me</span>
+
+      <div className="contact__container container grid">
+        <div className="contact__content">
+          <h3 className="contact__title">Talk to me</h3>
+
+          <div className="contact__info">
+            <div className="contact__card">
+              <i className="bx bx-mail-send contact__card-icon"></i>
+
+              <h3 className="contact__card-title">Email</h3>
+              <span className="contact__card-data">
+                farrezalhakim@gmail.com
+              </span>
+
+              <a
+                href="mailto:farrezalhakim@gmail.com"
+                className="contact__button"
+              >
+                Write me{" "}
+                <i className="bx bx_right-arrow-alt contact__button-icon"></i>
+              </a>
+            </div>
+
+            <div className="contact__card">
+              <i className="bx bxl-whatsapp contact__card-icon"></i>
+
+              <h3 className="contact__card-title">Whatsapp</h3>
+              <span className="contact__card-data">+6285884061543</span>
+
+              <a
+                href="https://api.whatsapp.com/send?phone=6285884061543&text=Hello, I would like to know more information about your services."
+                className="contact__button"
+              >
+                Write me{" "}
+                <i className="bx bx_right-arrow-alt contact__button-icon"></i>
+              </a>
+            </div>
+
+            <div className="contact__card">
+              <i className="bx bxl-messenger contact__card-icon"></i>
+
+              <h3 className="contact__card-title">Messenger</h3>
+              <span className="contact__card-data">farrez30</span>
+
+              <a href="https://m.me/farrez30" className="contact__button">
+                Write me{" "}
+                <i className="bx bx_right-arrow-alt contact__button-icon"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="contact__content">
+          <h3 className="contact__title">Write me your project</h3>
+
+          <form className="contact__form">
+            <div className="contact__form-div">
+              <label className="contact__form-tag">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="contact__form-input"
+                placeholder="Insert your name"
+              />
+            </div>
+            <div className="contact__form-div">
+              <label className="contact__form-tag">Mail</label>
+              <input
+                type="email"
+                name="email"
+                className="contact__form-input"
+                placeholder="Insert your email"
+              />
+            </div>
+            <div className="contact__form-div">
+              <label className="contact__form-tag">Project</label>
+              <textarea
+                name="project"
+                cols="30"
+                rows="10"
+                className="contact__form-input"
+                placeholder="Write your project"
+              ></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
